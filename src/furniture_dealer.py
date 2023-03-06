36244ae9276d45fd845b139e063d0709bc28c16e
PabloTejGar <pablo@MBP-de-Pablo.home>
2023-03-06 09:36:01 +0100

    Fix tests

--- src/furniture_dealer.py
+++ src/furniture_dealer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class FurnitureDealer:
 
     def __init__(self, size: int):
@@ -6,9 +8,13 @@ class FurnitureDealer:
 
     def calculate(self):
         result = []
+
         for index, item in enumerate(self.__options):
             rest = self.__size
-            result_by_option = self.create_combinations(index, rest, [])
+            result_by_option = []
+
+            if rest >= self.__options[index]:
+                result_by_option = self.create_combinations(index, rest, [])
 
             if len(result_by_option) != 0:
                 result.append(result_by_option)
@@ -24,7 +30,7 @@ class FurnitureDealer:
             rest = rest - option
             combinations.append(option)
             self.create_combinations(index, rest, combinations)
-        elif rest>= previousOption:
+        elif rest >= previousOption:
             rest = rest - previousOption
             combinations.append(previousOption)
             self.create_combinations(index-1, rest, combinations)
